a2033a25d472fcd1a93dda2ca8e4c3f3b2de575e
lolothens-e <annacast@espol.edu.ec>
2024-08-11 22:58:08 -0500

    Finalizacion y testeo de funcionalidades por consola
    -Integracion por interfaz
    -Mejoras menores en logica

--- Grupo_06_Adivina/src/main/java/modelo/Juego.java
+++ Grupo_06_Adivina/src/main/java/modelo/Juego.java
@@ -21,24 +21,28 @@ import java.util.Scanner;
  * @author Steven
  */
 public class Juego {
+    
+    static Scanner sc = new Scanner(System.in);
+    
+    static ArrayList<String> preguntas=leerPreguntas();
+    static HashMap<String,ArrayList<String>> respuestas=leerRespuestas();
+    
     public static ArbolBinario<String> arbolJuego = new ArbolBinario<>();
     public static ArrayList<String> respuestasUsuario=new ArrayList<>();
     public static int nPreguntasUsuario=0;
     
     public static int nPreguntas=leerPreguntas().size();
     public static HashMap<ArbolBinario,List<String>> caminos = cargarArbol();
-    public static HashMap<List<String>,String> animales = new HashMap<>();
+    public static HashMap<List<String>,String> animales = cargarAnimales();
     
-    static ArrayList<String> preguntas=leerPreguntas();
-    static HashMap<String,ArrayList<String>> respuestas=leerRespuestas();
-    
-    
-    public static void cargarAnimales(){
+    public static HashMap<List<String>,String> cargarAnimales(){
+        HashMap<List<String>,String> retorno=new HashMap<>();
         for(Map.Entry<ArbolBinario,List<String>> m: caminos.entrySet()){
-            if(!m.getKey().raiz.contenido.equals("Animal no definido")||!(((String)m.getKey().raiz.contenido).startsWith("¿"))){
-                animales.put(m.getValue(),(String)m.getKey().raiz.contenido );
+            if(!m.getKey().raiz.contenido.equals("Animal no definido")&&!(((String)m.getKey().raiz.contenido).startsWith("¿"))){
+                retorno.put(m.getValue(),(String)m.getKey().raiz.contenido );
             }
         }
+        return retorno;
     }
     
     //Carga arbolJuego con las preguntas y respuestas    
@@ -82,7 +86,6 @@ public class Juego {
                 else derecha=new ArbolBinario(s);
             } 
         }
-       
         local.put(derecha,new ArrayList<>(local.get(uso)));
         local.get(derecha).add("no");
         
@@ -100,35 +103,51 @@ public class Juego {
     
     
     public static void main(String []args){
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Ingrese numero de preguntas a jugar:");
-        nPreguntasUsuario=sc.nextInt();
+        System.out.print("Ingrese numero de preguntas a jugar o escribe 9999 para ir a modo trivia:");
+        Integer n=sc.nextInt();
+        if(n==9999) modoTrivia();
+        else juegoNormal(n);
+    }
+    
+    public static void juegoNormal(Integer n){
+        
         if(nPreguntasUsuario-nPreguntas<0)System.out.println("ADVERTENCIA: Ha seleccionado un numero de preguntas menor a la del archivo. No llegara a un animal especifico");
-        if(nPreguntasUsuario-nPreguntas>0)System.out.println("ADVERTENCIA: Ha seleccionado un numero de preguntas mayor a la del archivo. El juego no continuara tras llegar a un animal");
-        for(int i = 0; i<nPreguntasUsuario; i++){
-            System.out.println(preguntas.indexOf(i)+("Y/N"));
-            
-            String respuesta=sc.next();
-            
-            while(!"Y".equals(respuesta)||!"N".equals(respuesta)){
-                System.out.println("Opcion invalida. Ingrese nuevamente");
+        if(nPreguntasUsuario-nPreguntas>0)System.out.println("ADVERTENCIA: Ha seleccionado un numero de preguntas mayor a la del archivo. El juego no continuara tras llegar a un animal");   
+        
+        nPreguntasUsuario=n;
+        
+        preguntar(); 
+        
+        ArbolBinario respuesta = regresarArbolRespuesta(respuestasUsuario);
+        
+        System.out.println(respuestasUsuario);
                 
+        if(respuesta.esHoja()) System.out.println("Tu animal es: "+ (String)respuesta.raiz.contenido);
+        else System.out.println("No llegamos a una respuesta concluyente, sin embargo, tus animales pudieron haber sido:"+ regresarAnimalesDescendientes(respuesta));
+    }
+    
+    public static void preguntar(){
+        respuestasUsuario.clear();
+        for(int i = 0; i<nPreguntasUsuario; i++){
+            System.out.println(preguntas.get(i)+(" Responda con Y(Si) / N(No) :"));
+            
+            String respuesta=sc.next().strip().toLowerCase();
+            
+            while(!"y".equals(respuesta)&&!"n".equals(respuesta)){
+                System.out.println("Opcion invalida. Ingrese nuevamente");
+                respuesta=sc.next().strip().toLowerCase();
             }
-            if(respuesta.equals("Y"))respuestasUsuario.add("si");
-            if(respuesta.equals("N")) respuestasUsuario.add("no");
+            if(respuesta.equals("y"))respuestasUsuario.add("si");
+            if(respuesta.equals("n")) respuestasUsuario.add("no");
             
             if(respuestasUsuario.size()==nPreguntas) i=nPreguntasUsuario;
         }
-        ArbolBinario respuesta = regresarArbolRespuesta(respuestasUsuario);
-        
-        if(respuesta.esHoja()) System.out.println("Tu animal es:"+ (String)respuesta.raiz.contenido);
-        else System.out.println("No llegamos a una respuesta concluyente, sin embargo, tus animales pudieron haber sido:"+ regresarAnimalesDescendientes(respuesta));
     }
     
     public static ArrayList<String> regresarAnimalesDescendientes(ArbolBinario ab){
         ArrayList<String> regreso=new ArrayList<>();
         for(var e:ab.getDescendientes()){
-            boolean esAnimal= !((String)e).startsWith("¿")||!((String)e).equals("Animal no definido");
+            boolean esAnimal= !((String)e).startsWith("¿")&&!((String)e).equals("Animal no definido");
             if(esAnimal) regreso.add((String)e);
         }
         return regreso;
@@ -142,12 +161,52 @@ public class Juego {
         
         return null;
     }
+    
+    public static String capitalize(String s){
+        s=s.toLowerCase().strip();
+        s=s.substring(0,1).toUpperCase()+s.substring(1);
+        return s;
+    }
+    
+    public static void modoTrivia(){
+        ArrayList<String> claveAnimal=new ArrayList<>();
+        nPreguntasUsuario=nPreguntas;
+        
+        System.out.print("Bienvenido a trivia, escribe el nombre de un animal(existente) para comenzar: ");
+        String animalAdivina=sc.next();
+        
+        animalAdivina=capitalize(animalAdivina);
+        System.out.println(animalAdivina);
+        
+        while(!animales.values().contains(animalAdivina)) {
+            System.out.println(animalAdivina);
+            System.out.println("Animal no encontrado. Los animales disponibles son : "+ animales.values()+" \nIngrese de nuevo: ");
+            animalAdivina=capitalize(sc.next());  
+        }
+        
+        for(Map.Entry m:animales.entrySet())if(animalAdivina.equals(m.getValue())) claveAnimal=(ArrayList<String>)m.getKey();
+        
+        preguntar();
+        
+        System.out.println(respuestasUsuario);
+        
+        ArbolBinario respuesta=regresarArbolRespuesta(respuestasUsuario);
+        
+        if(respuesta.esHoja()&&respuesta.raiz.contenido.equals(animalAdivina)) System.out.println("Adivinaste correctamente el animal!");
+        else if(animales.getOrDefault(respuestasUsuario,"null").equals("null")){
+            respuestasUsuario.remove(respuestasUsuario.size()-1);
+            respuestasUsuario.remove(respuestasUsuario.size()-1);
+            System.out.println("No adivinaste el animal que buscabas. Aqui tienes algunos a los que quizas quisiste apuntar: "+ regresarAnimalesDescendientes(regresarArbolRespuesta(respuestasUsuario)));
+        }
+        else System.out.println("Aunque no adivinaste el animal "+animalAdivina+". Conseguiste atinarle a: "+ animales.get(respuestasUsuario));   
+    }
+    
     //Retorna una lista con las preguntas
     public static ArrayList<String> leerPreguntas(){
         ArrayList<String> lista= new ArrayList<>();
         
         try(BufferedReader leer= new BufferedReader(new FileReader("src/main/resources"
-        + "/archivos/Preguntas2.txt"))){
+        + "/archivos/Preguntas.txt"))){
             String linea= "";
             while((linea=leer.readLine())!=null){
                 lista.add(linea);
@@ -164,7 +223,7 @@ public class Juego {
         HashMap<String,ArrayList<String>> mapa= new HashMap<>();
         
         try(BufferedReader leer= new BufferedReader(new FileReader("src/main/resources"
-        + "/archivos/Respuestas2.txt"))){
+        + "/archivos/Respuestas.txt"))){
             String linea="";
             while((linea=leer.readLine())!=null){
                 ArrayList<String> lista= new ArrayList<>();
@@ -182,7 +241,4 @@ public class Juego {
         
         return mapa;
     }
-    
-    
-    
 }
